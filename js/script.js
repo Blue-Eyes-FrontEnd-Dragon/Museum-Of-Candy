02da951f568c8e4f508c6f24a7b2c511c20cdfe2
LukeMcCann <developer-lukemccann@outlook.com>
2021-08-24 13:14:18 +0100

    transform to arrow function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let nav = document.getElementById('mainNavbar');
 
-window.addEventListener('scroll', function () {
+window.addEventListener('scroll', () => {
     if (window.scrollY > 0) {
         nav.classList.add('scrolled');
     } else {
